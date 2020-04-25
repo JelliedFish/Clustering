d03d9c4d74e44503dbb580137e5f54d839d1c9c4
Bey Sviatoslav Igorevich <bey.sviatoslav@yandex.ru>
2020-04-26 01:18:01 +0300

    Code refactoring

--- Algorithm/Clasterization.py
+++ Algorithm/Clasterization.py
@@ -1,5 +1,4 @@
 import Algorithm.FindMST as mst
-import numpy as np
 
 edges = mst.findMst()
 
@@ -8,14 +7,9 @@ if edges == -1:
     print('Clastering cannot be perfomed.')
 else:
     # clustering preparation - creating an adjacency matrix
-    matrix = -1 * np.ones((len(edges), len(edges)))
-    for edge in edges:
-        matrix[edge[0]][edge[1]] = edge[2]
+    matrix = mst.mstAsMatrix(edges)
 
     condition = 0.01
-
-    # sorting edges in decreasing order
-    sorted(edges, key=lambda edge: edge[2], reverse=True)
 
     for edge in edges:
         if edge[2] > condition:
