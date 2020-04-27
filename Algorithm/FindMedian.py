4d88421cb4a52ae541630a676eb1a232170ddfa8
JelliedFish <hungrydog757@gmail.com>
2020-04-27 15:50:34 +0300

    v.1.1

--- Algorithm/FindMedian.py
+++ Algorithm/FindMedian.py
@@ -1,27 +1,32 @@
 import numpy as np
 import math
 
+
 def findMedian(matrix):
     median = 0.0
-    max = matrix[0][0]
-    min = matrix[0][0]
+
+    max_ = -10.0e777
+
+    min_ = 10.0e777
 
     M = len(matrix)
+
     for i in range(M):
         for j in range(M):
 
-            if matrix[i][j] > max:
-                max = matrix[i][j]
+            if matrix[i][j] > max_:
+                if matrix[i][j] != 0:
+                    max_ = matrix[i][j]
 
-            if matrix[i][j] < min:
-                min = matrix[i][j]
+            if matrix[i][j] < min_:
+                if matrix[i][j] != 0:
+                    min_ = matrix[i][j]
 
-    count_of_intervals = (math.ceil(math.log10(max)) - math.floor(math.log10(min)))
+    count_of_intervals = (math.ceil(math.log10(max_)) - math.floor(math.log10(min_)))
 
     if count_of_intervals % 2 == 1:
-        median = math.pow(10, math.ceil(count_of_intervals/2))
+        median = math.pow(10, math.ceil(count_of_intervals / 2))
     else:
-        median = math.pow(10, (count_of_intervals/2 + count_of_intervals/2 + 1)/2)
+        median = math.pow(10, (count_of_intervals / 2 + count_of_intervals / 2 + 1) / 2)
 
     return median
-
